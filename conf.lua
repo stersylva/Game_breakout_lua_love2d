27270110101d0dfb72022205640acfabd6c8b95e
stersylva <stersylva@gmail.com>
2017-05-31 20:19:03 -0300

    adicionando o config

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+	t.version = "0.10.1"
+	t.title = "Jogo Breakout em Lua"
+	t.window.width = 1000
+	t.window.height = 600
+	t.window.borderless = true
+	t.window.fullscreen = false
+end
